5e900aecea0b58951b69588e3d29596ed2aafcb8
Martin-Tarassov <>
2026-05-06 13:27:37 +0300

    fullstackInit

---
+++ FullStackReact.Server/Controllers/PlanetsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackReact.Server.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlanetsController : ControllerBase
+    {
+        public IActionResult SchoolIndex()
+        {
+            return Ok();
+        }
+    }
+}
